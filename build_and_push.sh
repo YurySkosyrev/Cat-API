298cba971dfd53310116fbb101323f3b02d2f63b
Aviaurez <skosyrev-87@mail.ru>
2022-08-15 23:52:40 +0300

    Cluster is created

--- build_and_push.sh
+++ build_and_push.sh
@@ -1,2 +1,8 @@
 ./gredlew clean build
 docker build . -t aviaurez/cats-api:1.0.0
+#docker push aviaurez/cats-api:1.0.0
+#docker pull aviaurez/cats-api:1.0.0
+
+docker run -ti --rm -e DATASOURCE_HOST=172.24.128.1 aviaurez/cats-api:1.0.0
+#-ti перехватываем сообщения консоли виртуальной машины в нашу консоль
+#--rm после остановки приложения контейнер удаляется
